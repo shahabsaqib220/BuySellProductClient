27c81000e8322e98f125135474f1763cdafeb133
shahabsaqib220 <shahabsaqib220@gmail.com>
2024-10-20 16:30:50 +0500

    Navbar Changed

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,37 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { Disclosure, Menu } from '@headlessui/react';
-
-import { FaCartArrowDown } from "react-icons/fa";
-import { RxAvatar } from "react-icons/rx";
+import { FaCartArrowDown } from 'react-icons/fa';
+import { RxAvatar } from 'react-icons/rx';
 import { NavLink, useNavigate } from 'react-router-dom';
 import useAxiosInstance from '../../ContextAPI/AxiosInstance';
-
 import { useAuth } from '../../ContextAPI/AuthContext'; // Assuming you have an AuthContext for managing auth state
 
 const Navbar = () => {
   const { isLoggedIn, logout, token } = useAuth(); // Getting auth state and token from context
   const [profileImage, setProfileImage] = useState(null); // State to store profile image URL
   const navigate = useNavigate(); // Hook for navigation
-  const [cartItemCount, setCartItemCount] = useState(0); 
-  const [isBadgeVisible, setIsBadgeVisible] = useState(false); 
-  const axiosInstance = useAxiosInstance(); 
-
-  // Define navigation array for mobile menu
- 
+  const [cartItemCount, setCartItemCount] = useState(0);
+  const axiosInstance = useAxiosInstance();
 
   // Utility function to conditionally apply class names
   const classNames = (...classes) => {
     return classes.filter(Boolean).join(' ');
   };
 
+
+  const intervalId = setInterval(() => {
+     if (isLoggedIn) {
+       fetchCartItemCount();
+     }
+   }, 5000);
+  
+
   useEffect(() => {
     // Fetch profile image if user is logged in
     const fetchProfileImage = async () => {
       if (isLoggedIn && token) {
         try {
-          // Use axiosInstance for API call
           const response = await axiosInstance.get('/profile-image/profile-image');
-    
+
           if (response.status === 200) {
             const data = response.data;
             setProfileImage(data.profileImageUrl); // Store the profile image URL in state
@@ -43,24 +44,13 @@ const Navbar = () => {
         }
       }
     };
-    
-  
+
     fetchProfileImage();
-  }, [isLoggedIn, token, axiosInstance]); // Include axiosInstance in the dependency array
-  
-
-
-  // const intervalId = setInterval(() => {
-  //   if (isLoggedIn) {
-  //     fetchCartItemCount();
-  //   }
-  // }, 5000);
+  }, [isLoggedIn, token, axiosInstance]);
 
   const fetchCartItemCount = async () => {
     try {
-      // Use axiosInstance for the API request
       const response = await axiosInstance.get('/usercart/item/cart');
-      
       if (response.status === 200) {
         const data = response.data;
         setCartItemCount(data.length); // Assuming data contains the cart items array
@@ -71,12 +61,10 @@ const Navbar = () => {
       console.error('Error fetching cart items:', error);
     }
   };
-  
 
   // Function to handle cart icon click
   const handleCartClick = () => {
     if (isLoggedIn) {
-      setIsBadgeVisible(false);
       navigate('/cart');
     } else {
       navigate('/login');
@@ -141,10 +129,13 @@ const Navbar = () => {
                   <>
                     {/* Cart Icon */}
                     <div className="relative">
-                      <FaCartArrowDown
+                      <button
                         onClick={handleCartClick}
-                        className="text-white w-8 h-8 cursor-pointer text-yellow-400"
-                      />
+                        className="text-black font-semibold py-2 px-4 rounded-lg"
+                      >
+                        <FaCartArrowDown className=" text-yellow-500  w-8 h-8" />
+                        
+                      </button>
                       {cartItemCount > 0 && (
                         <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-semibold rounded-full px-2 py-1">
                           {cartItemCount}
@@ -172,8 +163,6 @@ const Navbar = () => {
                             Dashboard
                           </NavLink>
                         </Menu.Item>
-                        
-                 
                         <Menu.Item>
                           <button
                             onClick={logout}
@@ -188,60 +177,91 @@ const Navbar = () => {
                 )}
               </div>
 
-              {/* Mobile menu button, Profile Image, and Cart Icon */}
+              {/* Mobile menu button */}
               <div className="sm:hidden flex items-center space-x-4">
-                {/* Cart Icon for mobile */}
-                <div className="relative">
-                  <FaCartArrowDown 
-                    className="text-yellow-500 text-3xl cursor-pointer"
-                    onClick={handleCartClick}
-                  />
-                  {cartItemCount > 0 && (
-                    <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-semibold rounded-full px-2 py-1">
-                      {cartItemCount}
-                    </span>
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {isOpen ? (
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16m-7 6h7"
+                      />
+                    </svg>
                   )}
-                </div>
-
-                {/* Profile Image for mobile */}
-                {isLoggedIn && (
-                  <Menu as="div" className="relative">
-                    <div>
-                      <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none">
-                        {profileImage ? (
-                          <img
-                            src={profileImage}
-                            alt="Profile"
-                            className="h-8 w-8 rounded-full object-cover"
-                          />
-                        ) : (
-                          <RxAvatar className="text-yellow-500 text-3xl" />
-                        )}
-                      </Menu.Button>
-                    </div>
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        <NavLink to="/profile" className="block px-4 py-2 text-sm text-gray-700">
-                          Dashboard
-                        </NavLink>
-                      </Menu.Item>
-                      
-                    
-                    
-                      <Menu.Item>
-                        <button
-                          onClick={logout}
-                          className="block w-full text-left px-4 py-2 text-sm text-gray-700"
-                        >
-                          Sign out
-                        </button>
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Menu>
-                )}
+                </Disclosure.Button>
               </div>
             </div>
           </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {!isLoggedIn ? (
+                <NavLink to="/login">
+                  <button
+                    type="button"
+                    className="bg-yellow-500 hover:bg-yellow-600 text-black font-semibold w-full py-2 px-4 rounded-lg"
+                  >
+                    Sign In
+                  </button>
+                </NavLink>
+              ) : (
+                <>
+                  {/* Cart Icon for mobile */}
+                  <div className="relative mb-2">
+                    <button
+                      onClick={handleCartClick}
+                      className="flex items-center justify-center w-full bg-yellow-500 hover:bg-yellow-600 text-black font-semibold py-2 px-4 rounded-lg text-left"
+                    >
+                      <FaCartArrowDown className="text-white mr-2" />
+                      <span>Cart</span>
+                    </button>
+                    {cartItemCount > 0 && (
+                      <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-semibold rounded-full px-2 py-1">
+                        {cartItemCount}
+                      </span>
+                    )}
+                  </div>
+                  <NavLink to="/profile">
+                    <button className="bg-yellow-500 hover:bg-yellow-600 text-black font-semibold w-full py-2 px-4 rounded-lg">
+                      Dashboard
+                    </button>
+                  </NavLink>
+                  <button
+                    onClick={logout}
+                    className="bg-yellow-500 hover:bg-yellow-600 text-black font-semibold w-full py-2 px-4 rounded-lg"
+                  >
+                    Log Out
+                  </button>
+                </>
+              )}
+            </div>
+          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
