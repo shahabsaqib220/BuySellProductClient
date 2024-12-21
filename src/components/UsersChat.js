8045686d4f090b8e68b48e80ee86b05458a8b90d
shahabsaqib220 <shahabsaqib220@gmail.com>
2024-12-21 19:38:56 +0500

    Workingon soeckets

--- src/components/UsersChat.js
+++ src/components/UsersChat.js
@@ -18,9 +18,20 @@ import useAxiosInstance from '../ContextAPI/AxiosInstance';// Adjust the path ba
 // http://localhost:5000
 // https://ols-server-eight.vercel.app
 
+
+
 const socket = io('https://ols-server-eight.vercel.app', {
-   transports: ["websocket", "polling"], // Ensure both WebSocket and polling are supported
+  transports: ["websocket", "polling"], // Ensure both WebSocket and polling are supported
 });
+
+
+
+
+
+
+
+
+
 
 
 const ReceiversList = () => {
