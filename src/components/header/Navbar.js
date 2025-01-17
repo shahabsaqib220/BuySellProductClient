b40d51a9fd444498db7df595500fc0e43227482e
shahabsaqib220 <shahabsaqib220@gmail.com>
2025-01-17 20:48:51 +0500

    has been

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -21,12 +21,12 @@ const Navbar = () => {
   const { t, i18n } = useTranslation();
   const dispatch = useDispatch();// Getting auth state and token from context
   const [profileImage, setProfileImage] = useState(null); // State to store profile image URL
+  const ad = useSelector((state) => state.user.ad);
   const navigate = useNavigate(); // Hook for navigation
   const { isLoggedIn, logout } = useAuth();
+  const axiosInstance = useAxiosInstance();
   const [cartItemCount, setCartItemCount] = useState(0);
   const user = useSelector((state) => state.user.user);
-  const axiosInstance = useAxiosInstance();
-  const ad = useSelector((state) => state.user.ad);
 
   // Utility function to conditionally apply class names
   const classNames = (...classes) => {
